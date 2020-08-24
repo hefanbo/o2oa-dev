6baf2bbba7a2ae04388c0cda2e13b2e788eed211
Fanbo He <hefanbo@gmail.com>
2020-08-24 21:27:55 +0800

    Try to build again after the first failure

--- tools/init.sh
+++ tools/init.sh
@@ -29,4 +29,15 @@ fi
 npm install -g gulp-cli
 npm install
 
+set +e
 /tools/build.sh
+ret=$?
+set -e
+
+if [ $ret -ne 0 ]; then
+  read -p "First build failed. Try again? (Y/n) " ans
+  ans=${ans:-Y}
+  if [ $ans = 'Y' ] || [ $ans = 'y' ]; then
+    /tools/build.sh
+  fi
+fi
